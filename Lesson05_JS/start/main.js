a2fc7421a41a87a55c4b84e97789bb71794311d6
FANDMNET\shin_sasaki <shin_sasaki@fandmnet.com>
2025-04-02 11:57:09 +0900

    07 条件分岐
    swichはrustのmatchと違い網羅性がなくてもエラーにならないので注意

--- Lesson05_JS/start/main.js
+++ Lesson05_JS/start/main.js
@@ -6,3 +6,33 @@ name = "鈴木　一郎";
 console.log(name);
 // > 鈴木　一郎
 
+/*
+let name = "山田　太郎";
+let name = "鈴木　一郎";
+
+console.log(name);
+// >Uncaught SyntaxError: Identifier 'name' has already been declared
+*/
+
+/*
+const name = "山田　太郎";
+name = "鈴木　一郎";
+
+console.log(name);
+// >Uncaught SyntaxError: Identifier 'name' has already been declared
+*/
+
+let color = "赤";
+switch (color) {
+case "赤":
+console.log("止まれ");
+break;
+case "黄":
+console.log("注意");
+break;
+case "緑":
+console.log("進め");
+break;
+default:
+console.log("無効な色です"); }
+// 出力: 止まれ
